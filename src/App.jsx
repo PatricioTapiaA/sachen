3b7521396316f7fd159fde5f6d4d10b36c71455e
Patricio Tapia <patricio.tapia@wefindyournewjob.com>
2025-01-15 21:33:17 +0100

    Configurando estructura base de Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import PitchDeck from '../pitch-deck/pitch-deck'
+
+function App() {
+  return (
+    <div className="App">
+      <PitchDeck />
+    </div>
+  )
+}
+
+export default App
